b942a6091bb84d5b29d83e48faa907f7bb514a81
Samuel Zambrano <szambranogarza@gmail.com>
2026-07-09 21:02:06 -0600

    Inserted data into de_project.stg_sales_order_header and validated row count and nulls

--- sql/01_create_schema.sql
+++ sql/01_create_schema.sql
@@ -2,9 +2,15 @@ USE AdventureWorks2025;
 GO
 
 
+-- =========================================================
 -- Create Schema
+-- =========================================================
 CREATE SCHEMA de_project;
 
+
+-- =========================================================
+-- Create staging tables
+-- =========================================================
 
 -- stg_sales_order_header
 -- Create Table 
@@ -20,3 +26,48 @@ CREATE TABLE de_project.stg_sales_order_header
 	TotalDue MONEY NOT NULL
 );
 GO
+
+-- Insert data into staging table
+INSERT INTO de_project.stg_sales_order_header
+(
+	SalesOrderID,
+	OrderDate,
+	CustomerID,
+	TerritoryID,
+	SubTotal,
+	TaxAmt,
+	Freight,
+	TotalDue
+)
+SELECT
+	SalesOrderID,
+	OrderDate,
+	CustomerID,
+	TerritoryID,
+	SubTotal,
+	TaxAmt,
+	Freight,
+	TotalDue
+FROM Sales.SalesOrderHeader;
+GO
+
+-- Validate tables
+-- Row Count
+SELECT COUNT(*) AS row_count_stg
+FROM de_project.stg_sales_order_header;
+
+SELECT COUNT(*) AS row_count_src
+FROM Sales.SalesOrderHeader;
+
+-- Check Nulls
+SELECT 
+	COUNT(*) as total_rows,
+	SUM(CASE WHEN SalesOrderID IS NULL THEN 1 ELSE 0 END) AS sales_order_id_nulls,
+	SUM(CASE WHEN OrderDate IS NULL THEN 1 ELSE 0 END) AS order_date_nulls,
+	SUM(CASE WHEN CustomerID IS NULL THEN 1 ELSE 0 END) AS customer_id_nulls,
+	SUM(CASE WHEN TerritoryID IS NULL THEN 1 ELSE 0 END) AS territory_id_nulls,
+	SUM(CASE WHEN SubTotal IS NULL THEN 1 ELSE 0 END) AS subtotal_nulls,
+	SUM(CASE WHEN TaxAmt IS NULL THEN 1 ELSE 0 END) AS tax_amt_nulls,
+	SUM(CASE WHEN Freight IS NULL THEN 1 ELSE 0 END) AS freight_nulls,
+	SUM(CASE WHEN TotalDue IS NULL THEN 1 ELSE 0 END) AS total_due_nulls
+FROM de_project.stg_sales_order_header;
